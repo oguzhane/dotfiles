9f3cfe4e1fb2595dda5fadf9d527a88346398d24
oguzhane <oguzhanergin13@gmail.com>
2025-09-01 00:53:27 +0100

    update dotfiles

--- dot_wsl.zsh
+++ dot_wsl.zsh
@@ -1,4 +1,4 @@
-export WINHOME=$(wslpath "$(wslvar USERPROFILE)")
+export WINHOME=$(wslpath "$(cmd.exe /C 'echo %USERPROFILE%' 2>/dev/null | tr -d '\r')")
 alias -g w='cd $WINHOME'
 alias -g cl='clear'
 cdw() {
